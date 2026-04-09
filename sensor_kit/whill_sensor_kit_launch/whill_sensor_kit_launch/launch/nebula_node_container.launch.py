78b4d91934b11ab8f510f75c96a50c42a0ed2256
Junya Sasaki <junya.sasaki@tier4.jp>
2026-04-09 20:34:58 +0900

    bug: fix to be `autoware_core` compatible
    Signed-off-by: Junya Sasaki <junya.sasaki@tier4.jp>

--- sensor_kit/whill_sensor_kit_launch/whill_sensor_kit_launch/launch/nebula_node_container.launch.py
+++ sensor_kit/whill_sensor_kit_launch/whill_sensor_kit_launch/launch/nebula_node_container.launch.py
@@ -39,24 +39,6 @@ def get_lidar_make(sensor_name):
     return "unrecognized_sensor_model"
 
 
-def get_vehicle_info(context):
-    # TODO(TIER IV): Use Parameter Substitution after we drop Galactic support
-    # https://github.com/ros2/launch_ros/blob/master/launch_ros/launch_ros/substitutions/parameter.py
-    gp = context.launch_configurations.get("ros_params", {})
-    if not gp:
-        gp = dict(context.launch_configurations.get("global_params", {}))
-    p = {}
-    p["vehicle_length"] = gp["front_overhang"] + gp["wheel_base"] + gp["rear_overhang"]
-    p["vehicle_width"] = gp["wheel_tread"] + gp["left_overhang"] + gp["right_overhang"]
-    p["min_longitudinal_offset"] = -gp["rear_overhang"]
-    p["max_longitudinal_offset"] = gp["front_overhang"] + gp["wheel_base"]
-    p["min_lateral_offset"] = -(gp["wheel_tread"] / 2.0 + gp["right_overhang"])
-    p["max_lateral_offset"] = gp["wheel_tread"] / 2.0 + gp["left_overhang"]
-    p["min_height_offset"] = 0.0
-    p["max_height_offset"] = gp["vehicle_height"]
-    return p
-
-
 def get_vehicle_mirror_info(context):
     path = LaunchConfiguration("vehicle_mirror_param_file").perform(context)
     with open(path, "r") as f:
@@ -65,10 +47,6 @@ def get_vehicle_mirror_info(context):
 
 
 def launch_setup(context, *args, **kwargs):
-    def load_composable_node_param(param_path):
-        with open(LaunchConfiguration(param_path).perform(context), "r") as f:
-            return yaml.safe_load(f)["/**"]["ros__parameters"]
-
     def create_parameter_dict(*args):
         result = {}
         for x in args:
@@ -135,54 +113,6 @@ def launch_setup(context, *args, **kwargs):
             remappings=[
                 ("pandar_points", "pointcloud_raw_ex"),
             ],
-            extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
-        )
-    )
-
-    cropbox_parameters = create_parameter_dict("input_pointcloud_frame", "input_frame", "output_frame", "processing_time_threshold_sec")
-    cropbox_parameters["negative"] = True
-
-    vehicle_info = get_vehicle_info(context)
-    cropbox_parameters["min_x"] = vehicle_info["min_longitudinal_offset"]
-    cropbox_parameters["max_x"] = vehicle_info["max_longitudinal_offset"]
-    cropbox_parameters["min_y"] = vehicle_info["min_lateral_offset"]
-    cropbox_parameters["max_y"] = vehicle_info["max_lateral_offset"]
-    cropbox_parameters["min_z"] = vehicle_info["min_height_offset"]
-    cropbox_parameters["max_z"] = vehicle_info["max_height_offset"]
-
-    nodes.append(
-        ComposableNode(
-            package="autoware_crop_box_filter",
-            plugin="autoware::crop_box_filter::CropBoxFilter",
-            name="crop_box_filter_self",
-            remappings=[
-                ("input", "pointcloud_raw_ex"),
-                ("output", "self_cropped/pointcloud_ex"),
-            ],
-            parameters=[cropbox_parameters],
-            extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
-        )
-    )
-
-    mirror_info = get_vehicle_mirror_info(context)
-    cropbox_parameters["min_x"] = mirror_info["min_longitudinal_offset"]
-    cropbox_parameters["max_x"] = mirror_info["max_longitudinal_offset"]
-    cropbox_parameters["min_y"] = mirror_info["min_lateral_offset"]
-    cropbox_parameters["max_y"] = mirror_info["max_lateral_offset"]
-    cropbox_parameters["min_z"] = mirror_info["min_height_offset"]
-    cropbox_parameters["max_z"] = mirror_info["max_height_offset"]
-
-    nodes.append(
-        ComposableNode(
-            package="autoware_crop_box_filter",
-            plugin="autoware::crop_box_filter::CropBoxFilter",
-            name="crop_box_filter_mirror",
-            remappings=[
-                ("input", "self_cropped/pointcloud_ex"),
-                ("output", "concatenated/pointcloud"),
-                #("output", "mirror_cropped/pointcloud_ex"),
-            ],
-            parameters=[cropbox_parameters],
             extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
         )
     )
